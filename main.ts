2ac503fd87a78ecdbe7e2848c9fc2d038e3b6baa
Mayank <9084735+mayank99@users.noreply.github.com>
2024-08-30 13:04:25 -0400

    fix trailing slash

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import { App, fsRoutes, staticFiles, trailingSlashes } from "fresh";
 import { type State } from "./utils.ts";
 
 export const app = new App<State>();
-app.use(staticFiles()).use(trailingSlashes("always"));
+app.use(staticFiles()).use(trailingSlashes("never"));
 
 await fsRoutes(app, {
   dir: "./",
